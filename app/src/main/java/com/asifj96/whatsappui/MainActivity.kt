0a8b374bea76d3da7908109918a98978ae4135f0
asifj96 <masifj1996@gmail.com>
2024-03-16 01:28:33 +0500

    initial commit

--- app/src/main/java/com/asifj96/whatsappui/MainActivity.kt
+++ app/src/main/java/com/asifj96/whatsappui/MainActivity.kt
@@ -3,13 +3,6 @@ package com.asifj96.whatsappui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.asifj96.whatsappui.ui.theme.WhatsappUITheme
 
 class MainActivity : ComponentActivity() {
